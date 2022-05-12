6f7851d01d24de4111f1d4c370200468f6833ac0
Mansoor nasir <1mansoor.nasir@gmail.com>
2022-05-12 15:51:49 +0500

    use $$

--- test/element.js
+++ test/element.js
@@ -1,11 +1,16 @@
 describe('interaction with webelement', () => {
     it('enter value in a field', async() => {
         await browser.url('/');
-        const job = await $('/*//a[@class="q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase header-link" and contains(., "Hire")]');
-        await job.click();
-        await browser.pause(6000);
-        const label = await $('div#title');
-        const text = await label.getText();
-        console.log(text)
+        // const job = await $('/*//a[@class="q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--no-uppercase header-link" and contains(., "Hire")]');
+        // await job.click();
+        // await browser.pause(6000);
+        // const label = await $('div#title');
+        // const text = await label.getText();
+        // console.log(text)
+        const menu = await $$('div.header-links > span > a.header-link').filter(async (el) => {
+            const text = await el.getText();
+            console.log(text);
+        });
+        
     })
 })
